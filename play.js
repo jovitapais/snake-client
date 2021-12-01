89e9e80f388f8819b9e65092ebe41288d888f888
Jovita <jovitaspais@gmail.com>
2021-12-01 21:44:17 +0000

    Client setup

---
+++ play.js
@@ -0,0 +1,27 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log("Am I being kicked out?");
+
+  });
+
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
